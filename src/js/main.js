639d7099dbbe94d5d3e7eb8536a0001bbc22d832
Vladyslav Mazurenko <mazur097mazur@gmail.com>
2022-05-04 00:15:55 +0200

    Okta throught proxy

--- src/js/main.js
+++ src/js/main.js
@@ -3,85 +3,91 @@ const SERVICES = [
 		name: 'JAMF',
 		statusWebsite: 'https://status.jamf.com/',
 		statusURL: ['https://status.jamf.com/api/v2/status.json'],
-		type: 'default',
+		type: 'description',
 	},
 	{
 		name: 'ASANA',
 		statusWebsite: 'https://status.asana.com/',
 		statusURL: ['https://status.asana.com/api/v2/status.json'],
-		type: 'default',
+		type: 'description',
 	},
 	{
-		name: 'Guru',
-		statusWebsite: 'https://status.getguru.com/',
-		statusURL: ['https://status.getguru.com/api/v2/status.json'],
-		type: 'default',
-	},
-	{
-		name: 'Zoom',
-		statusWebsite: 'https://status.zoom.us/',
-		statusURL: ['https://status.zoom.us/api/v2/status.json'],
-		type: 'default',
-	},
-	{
-		name: 'Slack',
-		statusWebsite: 'https://status.slack.com/',
-		statusURL: ['https://status.slack.com/api/v2.0.0/current'],
-		type: 'details',
-	},
-	{
-		name: '1Password',
-		statusWebsite: 'https://1password.statuspage.io/',
-		statusURL: ['https://1password.statuspage.io/api/v2/status.json'],
-		type: 'default',
-	},
-	{
-		name: 'Splunk',
-		statusWebsite: 'https://status.us0.signalfx.com/',
-		statusURL: ['https://status.us0.signalfx.com/api/v2/status.json'],
-		type: 'default',
-	},
-	{
-		name: 'Zendesk Services',
-		statusWebsite: 'https://status.zendesk.com/',
-		statusURL: ['https://status.zendesk.com/api/components/services?domain=support.zendesk.com'],
-		type: 'details',
-	},
-	{
-		name: 'Zendesk Support',
-		statusWebsite: 'https://status.zendesk.com/',
-		statusURL: ['https://status.zendesk.com/api/components/support?domain=support.zendesk.com'],
-		type: 'details',
-	},
-	{
-		name: 'Zendesk Chat',
-		statusWebsite: 'https://status.zendesk.com/',
-		statusURL: ['https://status.zendesk.com/api/components/chat?domain=support.zendesk.com'],
-		type: 'details',
-	},
-	{
-		name: 'Zendesk Explore',
-		statusWebsite: 'https://status.zendesk.com/',
-		statusURL: ['https://status.zendesk.com/api/components/explore?domain=support.zendesk.com'],
-		type: 'details',
-	},
-	{
-		name: 'Zendesk Sell',
-		statusWebsite: 'https://status.zendesk.com/',
-		statusURL: ['https://status.zendesk.com/api/components/sell?domain=support.zendesk.com'],
-		type: 'details',
-	},
-	{
-		name: 'Zendesk Sunshine',
-		statusWebsite: 'https://status.zendesk.com/',
-		statusURL: ['https://status.zendesk.com/api/components/sunshine?domain=support.zendesk.com'],
-		type: 'details',
+		name: 'Okta',
+		statusWebsite: 'https://trust.okta.com',
+		statusURL: ['https://trust.okta.com/api/status'],
+		type: 'data',
 	},
 	{
 		name: 'Google Workspace',
 		statusWebsite: 'https://www.google.com/appsstatus/dashboard/',
 		statusURL: ['https://www.google.com/appsstatus/dashboard/'],
 		type: 'google-table',
+	},
+	{
+		name: 'Guru',
+		statusWebsite: 'https://status.getguru.com/',
+		statusURL: ['https://status.getguru.com/api/v2/status.json'],
+		type: 'description',
+	},
+	{
+		name: 'Zoom',
+		statusWebsite: 'https://status.zoom.us/',
+		statusURL: ['https://status.zoom.us/api/v2/status.json'],
+		type: 'description',
+	},
+	{
+		name: 'Slack',
+		statusWebsite: 'https://status.slack.com/',
+		statusURL: ['https://status.slack.com/api/v2.0.0/current'],
+		type: 'status',
+	},
+	{
+		name: '1Password',
+		statusWebsite: 'https://1password.statuspage.io/',
+		statusURL: ['https://1password.statuspage.io/api/v2/status.json'],
+		type: 'description',
+	},
+	{
+		name: 'Splunk',
+		statusWebsite: 'https://status.us0.signalfx.com/',
+		statusURL: ['https://status.us0.signalfx.com/api/v2/status.json'],
+		type: 'description',
+	},
+	{
+		name: 'Zendesk Services',
+		statusWebsite: 'https://status.zendesk.com/',
+		statusURL: ['https://status.zendesk.com/api/components/services?domain=support.zendesk.com'],
+		type: 'status',
+	},
+	{
+		name: 'Zendesk Support',
+		statusWebsite: 'https://status.zendesk.com/',
+		statusURL: ['https://status.zendesk.com/api/components/support?domain=support.zendesk.com'],
+		type: 'status',
+	},
+	{
+		name: 'Zendesk Chat',
+		statusWebsite: 'https://status.zendesk.com/',
+		statusURL: ['https://status.zendesk.com/api/components/chat?domain=support.zendesk.com'],
+		type: 'status',
+	},
+	{
+		name: 'Zendesk Explore',
+		statusWebsite: 'https://status.zendesk.com/',
+		statusURL: ['https://status.zendesk.com/api/components/explore?domain=support.zendesk.com'],
+		type: 'status',
+	},
+	{
+		name: 'Zendesk Sell',
+		statusWebsite: 'https://status.zendesk.com/',
+		statusURL: ['https://status.zendesk.com/api/components/sell?domain=support.zendesk.com'],
+		type: 'status',
+	},
+	{
+		name: 'Zendesk Sunshine',
+		statusWebsite: 'https://status.zendesk.com/',
+		statusURL: ['https://status.zendesk.com/api/components/sunshine?domain=support.zendesk.com'],
+		type: 'status',
 	},
 ]
 
@@ -93,7 +99,7 @@ SERVICES.forEach(async (service) => {
 	const serviceItem = document.createElement('div')
 	serviceItem.classList.add('service')
 	servicesContainer.appendChild(serviceItem)
-	if (service.type === 'default' || service.type === 'details') {
+	if (service.type === 'description' || service.type === 'status' || service.type === 'data') {
 		serviceItem.innerHTML = `
 			<h3 class="service__title">
 				${service.name}
@@ -105,7 +111,7 @@ SERVICES.forEach(async (service) => {
 				return response.json()
 			})
 			.then((data) => {
-				if (service.type === 'default') {
+				if (service.type === 'description') {
 					serviceItem.innerHTML = `
 						<h3 class="service__title">
 							${service.name}
@@ -113,7 +119,7 @@ SERVICES.forEach(async (service) => {
 						<a href="${service.statusWebsite}" target="_blank" class="service__status service__status--okay">${data.status.description}</a>
 					`
 				}
-				if (service.type === 'details') {
+				if (service.type === 'status') {
 					serviceItem.innerHTML = `
 						<h3 class="service__title">
 							${service.name}
@@ -121,9 +127,17 @@ SERVICES.forEach(async (service) => {
 						<a href="${service.statusWebsite}" target="_blank" class="service__status service__status--okay">${data.status.toString()}</a>
 					`
 				}
+				if (service.type === 'data') {
+					serviceItem.innerHTML = `
+						<h3 class="service__title">
+							${service.name}
+						</h3>
+						<a href="${service.statusWebsite}" target="_blank" class="service__status service__status--okay">${data.data.status.toString()}</a>
+					`
+				}
 			})
 			.catch(err => {
-				console.log('Fetch error: ', err)
+				console.error(err)
 				serviceItem.innerHTML = `
 					<h3 class="service__title">
 						${service.name}
